efb9f82d255190b88f39e5199b8774d66dbd5831
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-25 10:49:06 +0700

    update-Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./index.css";
 
 function App() {
   
   return (
-    <div className="App">
-      
+    <div className="text-3xl font-bold underline">
+      Hello
     </div>
   )
 }
