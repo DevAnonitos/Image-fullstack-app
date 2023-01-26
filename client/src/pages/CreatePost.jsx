9eb25a6fbd5a24e451c3e1a82bce67a624361ba0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-26 09:09:26 +0700

    update-CreatePost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { preview } from "../assets";
+
+import { ArrowBackIcon } from '@chakra-ui/icons';
 
 import { getRandomPrompt } from '../utils';
 
@@ -8,8 +10,47 @@ import { FormField, Loader} from "../components";
 
 
 const CreatePost = () => {
+
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div>CreatePost</div>
+    <>
+      <section className='max-w-7xl mx-auto'>
+        <div>         
+          <h1 
+            className="font-extrabold 
+            text-[#222328] text-[32px]"
+          >
+            {/* <Link to='/'>
+              <ArrowBackIcon 
+                w={5} 
+                h={5} 
+                color="gray.500" 
+                className='flex justify-center items-center my-4'
+              />
+            </Link>    */}
+            Create
+          </h1>
+          <p 
+            className="mt-2 text-[#666e75] 
+            text-[14px] max-w-[500px]"
+          >
+            Generate an imaginative image through 
+            DALL-E AI and share it with the community
+          </p>
+        </div>
+      </section>
+    </>
   )
 }
 
