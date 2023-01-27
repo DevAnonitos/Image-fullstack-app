eb3f30a4cac194cb214f3feaae22851a5acf4211
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-27 22:09:12 +0700

    update-handle-func

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -33,11 +33,12 @@ const CreatePost = () => {
   };
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value});
   };
 
   const handleSurpriseMe = () => {
-   
+    const randomPrompt =getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt});
   };
 
   return (
