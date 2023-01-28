26020d588527794a4f51128c8f3e99b7b0b850ad
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-28 09:32:39 +0700

    update-BackendSettinng

--- server/index.js
+++ server/index.js
@@ -1,2 +1,22 @@
 import  express  from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', async(req, res) => {
+    res.send('Hello world!');
+});
+
+const startServer = async () => {
+    app.listen(8080, ()=> 
+        console.log('Server has started on port http://localhost:8080')
+    );
+};
+
+startServer();
