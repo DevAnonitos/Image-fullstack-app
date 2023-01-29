bca6cd7c7c308dda99eb7c50309f04541a722e78
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-29 08:44:35 +0700

    update-server-DallAPI@2

--- server/index.js
+++ server/index.js
@@ -11,9 +11,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-
+app.use(express.json())
 // app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
+
 
 app.use(express.json({limit: '50mb'}));
 
@@ -31,6 +32,7 @@ const startServer = async () => {
         );
     } catch (error) {
         console.log(error);
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
 };
 
