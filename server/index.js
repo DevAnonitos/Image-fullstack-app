a557e0d38d6854790ad7836c70c4919d75351bd1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-29 15:46:37 +0700

    update-Src-server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+app.use(express.urlencoded(
+    {   limit: '50mb', 
+        extended: true, 
+        parameterLimit: 50000,
+    }
+));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
@@ -33,7 +38,9 @@ const startServer = async () => {
         );
     } catch (error) {
         console.log(error);
-        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+        res.status(500).send(
+            error?.response.data.error.message || 'Something went wrong'
+        );
     }
 };
 
